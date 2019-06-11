85779844685250ccd337d72bbbdedb19f56eb406
makerspacequb <makerspace.qub@gmail.com>
2019-06-11 18:17:46 +0100

    Recieve Thread and Webpage Serial Monitor added

--- http_api/main.py
+++ http_api/main.py
@@ -10,6 +10,17 @@ import serial
 import time
 import os
 
+SerialPort = 'COM5' #'/dev/ttyACM0'
+Baudrate = 115200
+
+#define threading wrapper
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+    return wrapper
+
 try:
     
     class API(object):
@@ -38,6 +49,11 @@ try:
             log.write("Date and Time,Farmbot Response\n")
             log.close()
 
+            #Clear Webpage Serial Monitor
+            log = open("http_api/public/serialMonitor.txt","w")
+            log.write("Timestamp,Data\n")
+            log.close()
+            
             #Return Message
             status = currentDateTime + " - INFO: Transmit and Receive Logs have been cleared."
             print(status)
@@ -59,22 +75,11 @@ try:
                     log.write(currentDateTime+","+command+"\n")
 
                 #Write Command Passed to Serial Port
-                self.ser.reset_input_buffer()
-                payload = (str(command)+"\r\n").encode()
-                self.ser.write(payload)
-
-                #Read Response if Avalible
-                response = "VOID"
-                try:
-                    response = self.ser.readline().decode('utf-8')
-                except:
-                    response = "NULL\n"
-                    
-                #Add response to receive log
-                with open ("http_api/public/receiveLog.csv", "a+") as log:
-                    log.write(currentDateTime+","+str(response))
-
-                status = currentDateTime + " - INFO: '" + command + "' sent succesfully. Response is '"+response+"'."
+                self.serial.reset_output_buffer()
+                payload = (str(command)+"\n").encode()
+                self.serial.write(payload)
+                
+                status = currentDateTime + " - INFO: '" + command + "' sent succesfully."
 
             except:
                 status = currentDateTime + " - ERROR: Could not send '"+ command +"' to serial port. Check connection."
@@ -83,24 +88,67 @@ try:
             print(status)
             return status
 
+        @threaded
+        def receive(self):
+            
+            line = 0
+            log = open("http_api/public/serialMonitor.txt","w")
+            log.write("Timestamp,Data\n")
+            log.close()
+        
+            while self.connected == True:
+                
+                #Get Current Date and Time for Logging
+                currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+                #Read Response if Avalible
+                response = "VOID"
+                
+                try:
+                    if self.serial.in_waiting > 0:
+                        response = self.serial.readline().decode('utf-8')
+                    
+                        response = response.strip()
+                        logLine = currentDateTime+","+str(response)
+
+                        #Add response to receive log
+                        with open ("http_api/public/receiveLog.csv", "a+") as log:
+                            log.write(logLine+"\n")
+                        
+                        with open ("http_api/public/serialMonitor.txt", "a+") as log:
+                            log.write(logLine+"\n")
+                            line = line + 1
+
+                        print(logLine)
+                except:
+                    self.connected = False
+                    currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+                    status = currentDateTime + " - ERROR: Cannot read serial line."
+                    print(status)
+
         @cherrypy.expose
         def connect(self):
-            
-            currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
 
-            try:
-                #Open Serial Connection
-                self.ser = serial.Serial(
-                    port='\\.\COM8',
-                    baudrate=115200,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    bytesize=serial.EIGHTBITS
-                    )
-                self.connected = True
-                status = currentDateTime + " - INFO: Farmbot arduino connected to "+self.ser.name+"\n"
-            except:
-                status = currentDateTime + " - ERROR: Could not establish a connection with Arduino\n"
+            currentDateTime = time.strftime("%d/%m/%Y %H:%M:%S")
+            status = currentDateTime + " - INFO: Farmbot arduino already connected."
+
+            if(self.connected == False):
+                
+
+                try:
+                    #Open Serial Connection
+                    self.serial = serial.Serial(
+                        port= SerialPort,
+                        baudrate=Baudrate,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        bytesize=serial.EIGHTBITS,
+                        timeout=0.1
+                        )
+                    self.connected = True
+                    self.receive()
+                    status = currentDateTime + " - INFO: Farmbot arduino connected to "+self.serial.name+"\n"
+                except:
+                    status = currentDateTime + " - ERROR: Could not establish a connection with Arduino\n"
       
             print(status)
 
